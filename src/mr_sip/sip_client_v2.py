97de87371c0b6f7c248db8f0170577f1503cdba6
Jason Livesay <runvnc@gmail.com>
2025-10-08 05:08:39 -0500

    deepgram events

--- src/mr_sip/sip_client_v2.py
+++ src/mr_sip/sip_client_v2.py
@@ -516,7 +516,7 @@ class MindRootSIPBotV2(BareSIP):
         print("\033[91;107m[DEBUG TRACE 2/6] SIP client's _handle_turn_resumed callback triggered.\033[0m")
         
         # Set halt flag to stop TTS streaming immediately
-        self.session.halt_audio_out = True
+        #self.session.halt_audio_out = True
         #if self.context and self.context.log_id:
         #    from .sip_manager import get_session_manager
         #   session_manager = get_session_manager()
