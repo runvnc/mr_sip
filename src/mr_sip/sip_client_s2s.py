dc5ed6c05e5b9c2ae8b6ada5de729001d07164de
Jason Livesay <runvnc@gmail.com>
2025-11-18 02:15:11 -0600

    doesnt have to be 160

--- src/mr_sip/sip_client_s2s.py
+++ src/mr_sip/sip_client_s2s.py
@@ -328,7 +328,7 @@ class MindRootSIPBotS2S:
                 frame = audio_chunk[i:i+FRAME_SIZE]
                 
                 # Only send complete frames
-                if len(frame) == FRAME_SIZE:
+                if True or len(frame) == FRAME_SIZE:
                     try:
                         # Check interrupt flag BEFORE attempting to queue
                         #if self._interrupting:
